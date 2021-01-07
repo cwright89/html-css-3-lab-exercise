bb800a07460fb0c77e4006d5edb381c9113b36dd
cwright89 <connorwwright1989@gmail.com>
2021-01-07 16:54:07 -0700

    finished lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dropdownView: false
+    }
+  }
+
+  toggleDropdown = () => {
+    this.setState({dropdownView: !this.state.dropdownView})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="main-header">
+          <h1 classname='logo'>Start Bootstrap</h1>
+          <nav className='desktop-menu'>
+            <span>About</span>
+            <span>Projects</span>
+            <span>Contact</span>
+          </nav>
+          <div className='dropdown' onClick={this.toggleDropdown}>Menu</div>
+          {this.state.dropdownView
+            ? (
+              <nav className='mobile-menu'>
+                <span>About</span>
+                <span>Projects</span>
+                <span>Contact</span>
+              </nav>
+            )
+            : null}
+        </header>
+        <section className='main-content'>
+          <h1>Grayscale</h1>
+          <p>A free, responsive, one page Bootstrap theme <br></br>
+          created by Start Bootstrap</p>
+          <button className='get-started'>GET STARTED</button>
+        </section>
+
+      </div>
+    )
+  }
 }
 
 export default App;
